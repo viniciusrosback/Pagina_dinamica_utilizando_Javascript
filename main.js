a7d2197ee88676ec161b93647e3ad49b04864685
viniciusrosback <84256764+viniciusrosback@users.noreply.github.com>
2022-04-30 18:47:44 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+function tocaSom (seletorAudio){
+    const elemento = document.querySelector(seletorAudio);
+
+    if(elemento != null && elemento.localName === 'audio'){
+        elemento.play();    
+    }
+    else{
+        console.log('Elemento nao encontrado ou seletor invalido');
+        //alert('Elemento nao encontrado');
+    }
+    
+}
+
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+
+let contador = 0;
+
+// para
+for(let contador = 0; contador < listaDeTeclas.length; contador ++){
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; // template string
+
+    tecla.onclick = function(){
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function(evento){
+
+
+        if(evento.code === 'Space' || evento.code === 'Enter'){
+            tecla.classList.add('ativa');
+        }
+
+    }
+
+    tecla.onkeyup = function(){
+        tecla.classList.remove('ativa');
+    }
+
+}
+
